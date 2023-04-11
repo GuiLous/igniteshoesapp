c0c4fcaf39a9fdfca98ddc72587f307c1a8afa05
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-11 09:03:47 -0300

    build: initial setup
    setup inicial da aplicacao

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Routes } from './src/routes';
+
+import { THEME } from './src/theme';
+import { Loading } from './src/components/Loading';
+
+import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('ab15002f-8a09-49c2-bd62-acba2e1874ca');
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
