01f40cb0c559f10c6f067f7c7669b29594b98159
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-07-12 14:13:31 -0300

    update build

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
-import { StatusBar } from 'react-native';
-import OneSignal from 'react-native-onesignal';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
+import { useEffect } from 'react';
+import { Notification } from './src/components/Notification';
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 OneSignal.setAppId('ab15002f-8a09-49c2-bd62-acba2e1874ca');
 
+OneSignal.promptForPushNotificationsWithUserResponse()
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserInfoCreate()
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
+      console.log(notificationReceivedEvent)
+    })
+
+    return () => unsubscribe
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -25,6 +40,8 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      <Notification title="Mensagem de notificacao" onClose={() => { }} />
     </NativeBaseProvider>
   );
 }
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,5 @@
-import { HStack, Text, IconButton, CloseIcon, Icon } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { CloseIcon, HStack, Icon, IconButton, Text } from 'native-base';
 
 type Props = {
   title: string;
@@ -8,27 +8,27 @@ type Props = {
 
 export function Notification({ title, onClose }: Props) {
   return (
-    <HStack 
-      w="full" 
-      p={4} 
+    <HStack
+      w="full"
+      p={4}
       pt={12}
-      justifyContent="space-between" 
-      alignItems="center" 
+      justifyContent="space-between"
+      alignItems="center"
       bgColor="gray.200"
       position="absolute"
       top={0}
     >
-        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
+      <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2} />
 
-        <Text fontSize="md" color="black" flex={1}>
-          {title}
-        </Text>
+      <Text fontSize="md" color="black" flex={1}>
+        {title}
+      </Text>
 
-      <IconButton 
-        variant="unstyled" 
-        _focus={{ borderWidth: 0 }} 
-        icon={<CloseIcon size="3" />} 
-        _icon={{ color: "coolGray.600"}} 
+      <IconButton
+        variant="unstyled"
+        _focus={{ borderWidth: 0 }}
+        icon={<CloseIcon size="3" />}
+        _icon={{ color: "coolGray.600" }}
         color="black"
         onPress={onClose}
       />
